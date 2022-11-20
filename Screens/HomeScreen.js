875431c06d55dc9bf307ac3c6cef5ff9736ea871
zhukyu <phamtienhuu562002@gmail.com>
2022-11-20 12:41:23 +0700

    ui-home

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,37 +1,77 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import { auth } from '../firebase'
 import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import CustomListItem from '../components/CustomListItem'
+import { Avatar } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const HomeScreen = () => {
-  const navigation = useNavigation();
+const HomeScreen = ({ navigation }) => {
 
-  const handleSignOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        navigation.replace("Login")
-      })
-      .catch(error => alert(error.message))
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace('Login')
+    })
   }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "ChatApp",
+      headerTitleAlign: 'center',
+      headerLeft: () => (
+        <View >
+          <TouchableOpacity activeOpacity={0.5} onPress={signOutUser}>
+            {/* <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} /> */}
+            <Avatar
+              rounded
+              source={{
+                uri:
+                  'https://codefly.vn/wp-content/uploads/code/2020/12/11934/projecthtml/Source%20Code/uploadImage/Profile/blank_avatar.png',
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      ),
+      headerRight: () => (
+        <View style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width: 65,
+        }}>
+          <TouchableOpacity activeOpacity={0.5}>
+            <Icon name="camera" size={23} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.5}>
+            <Icon name="pencil" size={23} color="white" />
+          </TouchableOpacity>
+        </View>
+      ),
+    })
+  }, [navigation])
 
 
   return (
-    <View style={styles.container}>
-      <Text>Email: {auth.currentUser?.email}</Text>
-      <TouchableOpacity style={styles.button}
-        onPress={handleSignOut}
-      >
-        <Text style={styles.buttonText}>Sign out</Text>
-
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.mainBody}>
+      <ScrollView>
+        <CustomListItem />
+        <CustomListItem />
+        <CustomListItem />
+        <CustomListItem />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default HomeScreen
 
 const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: "#202B35",
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
