e72fc959f22fe1ac317a87903c0807fc4dc461fb
Tan Dung <tandung12072002@gmail.com>
2022-12-04 13:34:06 +0700

    createChat

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,70 +1,87 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { auth } from '../firebase'
-import { useNavigation } from '@react-navigation/native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import CustomListItem from '../components/CustomListItem'
-import { Avatar } from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { auth, db } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomListItem from "../components/CustomListItem";
+import { Avatar } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const HomeScreen = ({ navigation }) => {
-
+  const [chats, setChats] = useState([]);
   const signOutUser = () => {
     auth.signOut().then(() => {
-      navigation.replace('Login')
-    })
-  }
-
+      navigation.replace("Login");
+    });
+  };
+  useEffect(() => {
+    const unsubsribe = db.collection("chats").onSnapshot((snapshot) =>
+      setChats(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+    return unsubsribe;
+  }, []);
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "ChatApp",
-      headerTitleAlign: 'center',
+      headerTitleAlign: "center",
       headerLeft: () => (
-        <View >
+        <View>
           <TouchableOpacity activeOpacity={0.5} onPress={signOutUser}>
             {/* <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} /> */}
             <Avatar
               rounded
               source={{
-                uri:
-                  'https://codefly.vn/wp-content/uploads/code/2020/12/11934/projecthtml/Source%20Code/uploadImage/Profile/blank_avatar.png',
+                uri: "https://codefly.vn/wp-content/uploads/code/2020/12/11934/projecthtml/Source%20Code/uploadImage/Profile/blank_avatar.png",
               }}
             />
           </TouchableOpacity>
         </View>
       ),
       headerRight: () => (
-        <View style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: 65,
-        }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 65,
+          }}
+        >
           <TouchableOpacity activeOpacity={0.5}>
             <Icon name="camera" size={23} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity activeOpacity={0.5}
-          onPress={()=> navigation.navigate("AddChat")}>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => navigation.navigate("AddChat")}
+          >
             <Icon name="pencil" size={23} color="white" />
           </TouchableOpacity>
         </View>
       ),
-    })
-  }, [navigation])
-
+    });
+  }, [navigation]);
 
   return (
     <SafeAreaView style={styles.mainBody}>
-      <ScrollView>
-        <CustomListItem />
-        <CustomListItem />
-        <CustomListItem />
-        <CustomListItem />
+      <ScrollView style={{height: '100%'}}>
+        {chats.map(({id, data: {chatName}}) => (
+          <CustomListItem key={id} id={id} chatName={chatName}/>
+        ))}
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   mainBody: {
@@ -75,20 +92,20 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   button: {
-    backgroundColor: '#0782F9',
-    width: '60%',
+    backgroundColor: "#0782F9",
+    width: "60%",
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 40,
   },
   buttonText: {
-    color: 'white',
-    fontWeight: '700',
+    color: "white",
+    fontWeight: "700",
     fontSize: 16,
-  }
-})
+  },
+});
